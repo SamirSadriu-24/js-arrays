7c624cef9ec9433d2458aa146ebde45e11be1135
AidenYukino <samirsadriuboolean@gmail.com>
2025-08-31 15:25:52 +0200

    es 6-7-8

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,11 +10,11 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = console.log(teachers[3]);
+const fourthTeacher = (teachers[3]);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers[5] = "Patrick";
-console.log(teachers[5]);
+teachers[4] = "Patrick";
+console.log(teachers[4]);
 console.log(teachers);
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop();
@@ -28,11 +28,22 @@ console.log(teachers);
 teachers.push("Vanessa");
 console.log(teachers);
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift("Sarah");
+console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = (teachers.indexOf("Lewis"));
+console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+let isTeachersEmpty
+
+if (teachers.length === 0){
+  isTeachersEmpty = true;
+} else {
+  isTeachersEmpty = false;
+}
+
+console.log(isTeachersEmpty);
